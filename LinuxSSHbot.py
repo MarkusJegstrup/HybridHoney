12ab97f763e790dbb40ecdb70a6d3f865bf13592
MJEG <m.e.h.jegstrup@gmail.com>
2024-12-16 12:40:42 +0100

    refactored non-methods

--- LinuxSSHbot.py
+++ LinuxSSHbot.py
@@ -11,12 +11,16 @@ import sudoPass
 from dotenv import load_dotenv
 import sys
 
+##Global Fields
+full_command = ""
 main_command =""
 args = []
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 username = ""
 attacker_ip = ""
 first_prompt = True
+history = open(os.path.join(BASE_DIR, "history.txt"), "a+", encoding="utf-8")
+history.truncate(0)
 
 def readline_input(prompt):
     try:
@@ -27,21 +31,26 @@ def readline_input(prompt):
         exit(0)
         
 # Get the SSH connection details from the environment
-ssh_connection = os.getenv("SSH_CONNECTION", "")
+#ssh_connection = os.getenv("SSH_CONNECTION", "")
 
-if ssh_connection:
-    # Extract the attacker's IP address (first field in SSH_CONNECTION)
-    attacker_ip = ssh_connection.split()[0]
-    username =  os.getlogin( )
-    # print(f"Attacker IP Address: {attacker_ip}")
-else:
-    print("IP address unreachable")
+def username_att_ip(ssh_connection):
+    global attacker_ip
+    global username
+    if ssh_connection:
+        # Extract the attacker's IP address (first field in SSH_CONNECTION)
+        attacker_ip = ssh_connection.split()[0]
+        username =  os.getlogin( )
+        # print(f"Attacker IP Address: {attacker_ip}")
+    else:
+        print("IP address unreachable")
 
 def handle_cmd(cmd):
-    parts = cmd.split()
+    global full_command
     global main_command
-    main_command = parts[0]
     global args
+    full_command = cmd
+    parts = cmd.split()
+    main_command = parts[0]
     args = parts[1:]
 
 
@@ -59,44 +68,57 @@ def plugin_pre_handler(cmd):
 def plugin_post_handler(message):
     return message 
 
-# Load environment variables from the .env file
-load_dotenv(dotenv_path=os.path.join(BASE_DIR, ".env"))
+def setup():
+    # Load environment variables from the .env file
+    load_dotenv(dotenv_path=os.path.join(BASE_DIR, ".env"))
 
-# Set the OpenAI API key
-openai.api_key = os.getenv("OPENAI_API_KEY")
+    # Set the OpenAI API key
+    openai.api_key = os.getenv("OPENAI_API_KEY")
 
-if not openai.api_key:
-    raise ValueError("OPENAI_API_KEY is not set or not loaded from the .env file.")
+    if not openai.api_key:
+        raise ValueError("OPENAI_API_KEY is not set or not loaded from the .env file.")
+    
+    # config = dotenv_values(".env")
+    # openai.api_key = config["OPENAI_API_KEY"]
 
-# config = dotenv_values(".env")
-# openai.api_key = config["OPENAI_API_KEY"]
-today = datetime.now()
-random_days = random.randint(0, 5)
-random_hours = random.randint(0, 23)
-random_minutes = random.randint(0, 59)
-random_seconds = random.randint(0, 59)
-last_login = today - timedelta(days=random_days, hours=random_hours, minutes=random_minutes, seconds=random_seconds)
-random_ip = ".".join(map(str, (random.randint(0, 255) 
-                        for _ in range(4))))
 
-history = open(os.path.join(BASE_DIR, "history.txt"), "a+", encoding="utf-8")
-history.truncate(0)
+def last_login_random_ip():
+    today = datetime.now()
+    random_days = random.randint(0, 5)
+    random_hours = random.randint(0, 23)
+    random_minutes = random.randint(0, 59)
+    random_seconds = random.randint(0, 59)
+    last_login = today - timedelta(days=random_days, hours=random_hours, minutes=random_minutes, seconds=random_seconds)
+    random_ip = ".".join(map(str, (random.randint(0, 255) 
+                            for _ in range(4))))
+    return last_login, random_ip
 
-if os.stat(os.path.join(BASE_DIR, "history.txt")).st_size == 0:
-    with open(os.path.join(BASE_DIR, "personalitySSH.yml"), 'r', encoding="utf-8") as file:
-        identity = yaml.safe_load(file)
+def prompt_setup():
+    prompt = ""
+    if os.stat(os.path.join(BASE_DIR, "history.txt")).st_size == 0:
+        with open(os.path.join(BASE_DIR, "personalitySSH.yml"), 'r', encoding="utf-8") as file:
+            identity = yaml.safe_load(file)
 
-    identity = identity['personality']
+        identity = identity['personality']
 
-    prompt = identity['prompt']
+        prompt = identity['prompt']
 
-else:
-    history.write("\nHere the session stopped. Now you will start it again from the beginning with the same user. You must respond just with starting message and nothing more. " +
-                              "Make sure you use same file and folder names. Ignore date-time in <>. This is not your concern.\n")
-    history.seek(0)
-    prompt = history.read()
+    else:
+        history.write("\nHere the session stopped. Now you will start it again from the beginning with the same user. You must respond just with starting message and nothing more. " +
+                                "Make sure you use same file and folder names. Ignore date-time in <>. This is not your concern.\n")
+        history.seek(0)
+        prompt = history.read()
+    return prompt
 
 def main():
+    ###Setup
+    ssh_connection = os.getenv("SSH_CONNECTION", "")
+    username_att_ip(ssh_connection)
+    setup()
+    last_login, random_ip = last_login_random_ip()
+    
+    prompt = prompt_setup()
+
     parser = argparse.ArgumentParser(description = "Simple command line with GPT-3.5-turbo")
     parser.add_argument("--personality", type=str, help="A brief summary of chatbot's personality", 
                         default= prompt + 
