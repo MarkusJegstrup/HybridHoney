125899043eaeea014c71f56bbf6aa38a4dc024b7
Simon Bang Hjortkilde <simonhjortkilde@gmail.com>
2025-01-06 14:52:12 +0100

    base_DIR

--- sudoPass.py
+++ sudoPass.py
@@ -8,16 +8,9 @@ def handle_fake_sudo_give_access():
     max_attempts = 3
     is_password = False
 
-    import getpass
-
-def handle_fake_sudo_give_access():
-    attempts = 0
-    max_attempts = 3
-    is_password = False
-
     # Load correct passwords from a file
     try:
-        with open(os.path.join(BASE_DIR, "10kpasswords.txt"), "a+", encoding="utf-8") as file:
+        with open(os.path.join(BASE_DIR, "10kpasswords.txt"), "r", encoding="utf-8") as file:
             correct_passwords = [line.strip() for line in file]  # Remove any leading/trailing whitespace
     except FileNotFoundError:
         print("Error: 10kpasswords.txt file not found.")
@@ -40,11 +33,6 @@ def handle_fake_sudo_give_access():
     # After max attempts, show failure message
     if is_password == False:
         print("sudo: {} incorrect password attempts".format(max_attempts))
-
-    # Write captured attempts to a log file for further analysis
-    #with open("/tmp/honeypot_password_log.txt", "a") as log_file:
-    #    for attempt_num, captured_password in enumerate(captured_passwords, 1):
-    #        log_file.write(f"Attempt {attempt_num}: {captured_password}\n")
 
     return is_password
 
