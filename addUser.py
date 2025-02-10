dddfaf49a08d1b81f664c3487e301cf48a735dc7
Simon Bang Hjortkilde <simonhjortkilde@gmail.com>
2025-02-10 16:26:17 +0100

    useradd test

--- addUser.py
+++ addUser.py
@@ -19,7 +19,7 @@ def handle_useradd(command):
             #+ ["-g", "redirect"]
         print(f"Executing: {' '.join(updated_command)}")
 
-        result = subprocess.run(updated_command, capture_output=True, text=True)
+        result = subprocess.run(updated_command, shell=True, capture_output=True, text=True)
 
         if result.returncode == 0:
             ""
